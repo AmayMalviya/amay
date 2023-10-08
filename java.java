c6a34ca7b1780b1a0059ac54508f58c84aba7226
AmayMalviya <145102965+AmayMalviya@users.noreply.github.com>
2023-10-08 22:29:34 +0530

    Add files via upload
    my first git upload

---
+++ java.java
@@ -0,0 +1,16 @@
+import java.lang.*
+import java.util.Scanner
+
+class rucursion{
+int r, num, n=0;
+r = num%10;
+n += num;
+}
+class main{
+public static void main(string[] args){
+Scanner sc = new Scanner(System.in);
+recursion r = new recursion();
+int s = nextInt(sc);
+r.num = s;
+}
+}
